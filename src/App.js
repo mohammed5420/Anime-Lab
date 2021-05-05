dbadd02185810a72e91d118d405f5b6a59a2ad4e
mohammed5420 <almoaz067@gmail.com>
2021-05-05 01:07:38 -0700

    fixing random colors functionalty

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Search from "./pages/Search";
 const App = () => {
   return (
     <div className="container mx-auto px-2 sm:px-3 md:px-4 lg:px-6">
-      <Switch>
+      <Switch >
         <Route path="/" exact>
           <Home />
         </Route>
@@ -19,6 +19,14 @@ const App = () => {
           <Details />
         </Route>
       </Switch>
+      <div className="bg-red-400"></div>
+      <div className="bg-indigo-400"></div>
+      <div className="bg-gray-400"></div>
+      <div className="bg-blue-400"></div>
+      <div className="bg-yellow-400"></div>
+      <div className="bg-green-400"></div>
+      <div className="bg-purple-400"></div>
+      <div className="bg-pink-400"></div>
     </div>
   );
 };
